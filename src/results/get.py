f5e6763ba742ca202547bc69d86187727ef0dc1d
John Forbes <john.robert.forbes@gmail.com>
2024-03-10 21:38:37 +1100

    Removed ./src/results/get.py.

--- src/results/get.py
+++
@@ -1,6 +0,0 @@
-def f(x):
-  s = x['service']
-  messages = s.users().messages()
-  _x = {'userId':'me', 'labelIds':['INBOX']}
-  if 'page_token' in x: _x['pageToken'] = x['page_token']
-  return messages.list(**_x).execute()
