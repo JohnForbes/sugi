a428621f452db7043b864fa917d890f66d402e39
John Forbes <john.robert.forbes@gmail.com>
2024-03-10 21:36:29 +1100

    Updated ./gitter.py.

--- gitter.py
+++ gitter.py
@@ -1,2 +1,8 @@
-from xgu.main import f as main
-if __name__ == '__main__': main(True)
+from sys import argv
+from f.gitter.main import f as main
+from hak.file.load import f as load
+from hak.file.save import f as save
+
+if __name__ == '__main__':
+  main(argv[-1] != '0')
+  save('gitter.py', load('gitter.py'))
